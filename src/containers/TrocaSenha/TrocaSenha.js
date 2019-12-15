252f3b269a297e85b3802a77eb3f6987e102dfce
rogerio bispo <rogerio_pd@yahoo.com.br>
2019-12-15 08:21:02 -0300

    validation on all forms

--- src/containers/TrocaSenha/TrocaSenha.js
+++ src/containers/TrocaSenha/TrocaSenha.js
@@ -1,72 +1,124 @@
-import React, { useState } from "react";
-import { Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
-import { Redirect } from 'react-router'
+import React, { Redirect } from "react";
+import { Form, FormGroup, Col, ControlLabel, FormControl, Tooltip, Button} from "react-bootstrap"
+import { Formik } from "formik";
+import * as Yup from "yup";
+
 
 import AuthClient from "../../services/authentication/AuthClient"
 import { user } from '../../services/authentication/auth'
 import "./TrocaSenha.css";
 
-export default function TrocaSenha(props){
-    const [password1, setpassword1] = useState("")
-    const [password2, setPassword2] = useState("")
-    const [redir, setRedir] = useState(false)
-  
-  
-  function validateForm() {
-    return  password1 !== password2
-  }
 
- async function  handleSubmit(event) {
-    event.preventDefault()
-    if(validateForm()){ 
-      window.flash('As Senhas sâo diferentes', 'error')
-      return
-    }
-    try {
-        await AuthClient.post("/users/password", 
-        { "userName": user().userName, 	"newPassword": password1 })
-       
-         window.flash(`Senha alterada com sucesso`, 'success')
-         setRedir(true)
-         
-        } catch (e) {
-      console.log(e)
-      window.flash(`Não foi possivel alterar o password`, 'error')
-    }
-    
-  }
-  if(redir) return <Redirect to="/home" />
+
+export default function trocaSenha(props) {
     return (
       <div className="Home">
-      <div className="lander">
-      
-        <div className="Login">
-          <form  onSubmit={handleSubmit}>
-            <FormGroup controlId="password1" bsSize="large">
-              <ControlLabel>Senha</ControlLabel>
-              <FormControl
-                autoFocus
-                type="password"
-                value={password1}
-                onChange={e => setpassword1(e.target.value)}
-              />
-            </FormGroup>
-            <FormGroup controlId="password" bsSize="large">
-              <ControlLabel>Confirmação de senha</ControlLabel>
-              <FormControl
-                value={password2}
-                onChange={e => setPassword2(e.target.value)}
-                type="password"
-              />
-            </FormGroup>
-            <Button block bsSize="large" type="submit">
-              Alterar
-            </Button>
-          </form>
-        </div>
+        <div className="lander">
+       <Formik
+          initialValues={{ password: "", confirmPassword: ""}}
+          onSubmit={async values => {
+            try {
+                console.dir(props)
+                await AuthClient.post("/users/password", 
+                { "userName": user().userName, 	"newPassword": values.password })
+                 window.flash(`Senha alterada com sucesso`, 'success')
+                } catch (e) {
+               window.flash(`Não foi possivel alterar o password`, 'error')
+            }
+          }}
+          validationSchema={Yup.object().shape({
+            password: Yup.string()
+              .min(5, 'Senha deve ser maior que 5')
+              .required("password é obrigatório"),
+            confirmPassword: Yup.string()
+              .min(5, 'Senha deve ser maior que 5')
+              .required("password é obrigatório")
+              .test('is-true', 'Password divergente', function(confirmPassword){ return this.parent.password === confirmPassword })
+          })}
+        >
+          {props => {
+            const {
+              values,
+              touched,
+              errors,
+              handleChange,
+              handleBlur,
+              isSubmitting,
+              handleSubmit,
+            } = props;
+            return (
 
+              <div className="Home">
+                  <div className="lander">
+
+                      <Form horizontal onSubmit={handleSubmit}>
+                          {/* inicio Password */}
+                              <FormGroup controlId="password">
+                                <Col componentClass={ControlLabel} sm={2}>
+                                  Senha
+                                </Col>
+                                <Col sm={10}>
+                                  <FormControl id="password"
+                                              placeholder="senha"
+                                              type="password"
+                                              value={values.password}
+                                              onChange={handleChange}
+                                              onBlur={handleBlur}
+                                              className={
+                                              errors.password && touched.password ? (
+                                                'text-input error'
+                                              ) : (
+                                                'text-input'
+                                              )
+                                  } />
+                                  {errors.password && touched.password && (
+                                    <Tooltip placement="bottom" className="in" id="tooltip-right">{errors.password}</Tooltip>
+                                  )}
+                                </Col>
+                              </FormGroup>
+                          {/* fim Password */}
+                          {/* inicio Password2 */}
+                          <FormGroup controlId="confirmPassword">
+                                <Col componentClass={ControlLabel} sm={2}>
+                                  Confirmação Senha
+                                </Col>
+                                <Col sm={10}>
+                                  <FormControl id="confirmPassword"
+                                              placeholder="Confirmar senha"
+                                              type="password"
+                                              value={values.confirmPassword}
+                                              onChange={handleChange}
+                                              onBlur={handleBlur}
+                                              className={
+                                              errors.confirmPassword && touched.confirmPassword ? (
+                                                'text-input error'
+                                              ) : (
+                                                'text-input'
+                                              )
+                                  } />
+                                  {errors.confirmPassword && touched.confirmPassword && (
+                                    <Tooltip placement="bottom" className="in" id="tooltip-right">{errors.confirmPassword}</Tooltip>
+                                  )}
+                                </Col>
+                              </FormGroup>
+                          {/* fim Password */}
+                          {/* Inicio botoes */}
+                              <FormGroup>
+                                <Col smOffset={0} sm={0}>
+                                  <Button type="submit" bsStyle="primary" bsSize="large" disabled={isSubmitting}>Alterar</Button> 
+                                </Col>
+                              </FormGroup>
+                          {/* fim botoes */}
+                      </Form>
+                  </div>
+              </div>
+            );
+          }}
+        </Formik>
+        
+        
+        </div>
       </div>
-    </div>
-    )
-  
+    );
+
 }
--- src/containers/UsuarioForm/UsuarioForm.js
+++ src/containers/UsuarioForm/UsuarioForm.js
@@ -1,83 +1,237 @@
 import React from "react";
-import { Form, FormGroup, Col, ControlLabel, FormControl, Checkbox, Button} from "react-bootstrap"
-import "./UsuarioForm.css";
+import { Form, FormGroup, Col, ControlLabel, FormControl, Tooltip, Button} from "react-bootstrap"
+
+import { Formik } from "formik";
+import * as Yup from "yup";
 
 export default function UsuarioForm() {
-  return (
-    <div className="Home">
-      <div className="lander">
-        {/* Inicio form */}
-        {/* campo nome */}
-        <Form horizontal>
-          <FormGroup controlId="formHorizontalName">
-            <Col componentClass={ControlLabel} sm={2}>
-              Nome
-            </Col>
-            <Col sm={10}>
-              <FormControl type="text" placeholder="Primeiro Nome" />
-            </Col>
-          </FormGroup>
-          {/* fim campo nome */}
+    return (
+      <div className="Home">
+        <div className="lander">
+       <Formik
+          initialValues={{ email: "", nome: "", sobreNome: "", usuario: "", password: "", confirmPassword: "", admin: false}}
+          onSubmit={async user => {
+            await new Promise(resolve => setTimeout(resolve, 500));
+            alert(JSON.stringify(user, null, 2));
+          }}
+          validationSchema={Yup.object().shape({
+            nome: Yup.string()
+              .min(3, "Nome menor que 3 caracteres")
+              .required("Nome é Obrigatório"),
+            sobreNome: Yup.string()
+              .min(3, "sobreNome menor que 3 caracteres")
+              .required("sobreNome é Obrigatório"),
+            usuario: Yup.string()
+              .matches(/^[a-z]+\.[a-z]+$/, {
+                message:'formato errado exemplo: beltrano.silva',
+                excludeEmptyString: true
+              })
+              .lowercase('Apenas minusculo')
+              .required("usuario é Obrigatório"),
+            email: Yup.string()
+              .email()
+              .required("Email Obrigatório"),
+            password: Yup.string()
+              .min(5, 'Senha deve ser maior que 5')
+              .required("password é obrigatório"),
+            confirmPassword: Yup.string()
+              .min(5, 'Senha deve ser maior que 5')
+              .required("password é obrigatório")
+              .test('is-true', 'Password divergente', function(confirmPassword){ return this.parent.password === confirmPassword })
+          })}
+        >
+          {props => {
+            const {
+              values,
+              touched,
+              errors,
+              dirty,
+              isSubmitting,
+              handleChange,
+              handleBlur,
+              handleSubmit,
+              handleReset
+            } = props;
+            return (
+              <Form horizontal onSubmit={handleSubmit}>
+                {/* campo nome */}
+                  <FormGroup controlId="nome">
+                    <Col componentClass={ControlLabel} sm={2}>
+                      Nome
+                    </Col>
+                    <Col sm={10}>
+                      <FormControl id="nome"
+                                   placeholder="Nome"
+                                   type="text"
+                                   value={values.nome}
+                                   onChange={handleChange}
+                                   onBlur={handleBlur}
+                                   className={
+                                     errors.nome && touched.nome
+                                       ? "text-input error"
+                                       : "text-input"
+                      } />
+                     {errors.nome && touched.nome && (
+                       <Tooltip placement="bottom" className="in" id="tooltip-right">{errors.nome}</Tooltip>
+                    )}
 
-          {/* campo Sobrenome */}  
-          <FormGroup controlId="formHorizontalLastName">
-              <Col componentClass={ControlLabel} sm={2}>
-                SobreNome
-              </Col>
-              <Col sm={10}>
-                <FormControl type="text" placeholder="SobreNome" />
-              </Col>
-          </FormGroup>
-          {/* fim campo Sobrenome */}
-          
-          {/* campo usuário */}  
-          <FormGroup controlId="formHorizontalUser">
-              <Col componentClass={ControlLabel} sm={2}>
-                Usuário
-              </Col>
-              <Col sm={10}>
-                <FormControl type="text" placeholder="Usuário" />
-              </Col>
-          </FormGroup>
-          {/* fim campo usário */}
-          {/* inicio Email */}
-          <FormGroup controlId="formHorizontalEmail">
-            <Col componentClass={ControlLabel} sm={2}>
-              Email
-            </Col>
-            <Col sm={10}>
-              <FormControl type="email" placeholder="Email" />
-            </Col>
-          </FormGroup>
-          {/* fim Email */}
+                    </Col>
+                  </FormGroup>
+                  {/* fim campo nome */}
+                  {/* campo ultimo nome */}
+                  <FormGroup controlId="sobreNome">
+                    <Col componentClass={ControlLabel} sm={2}>
+                      Sobre Nome
+                    </Col>
+                    <Col sm={10}>
+                      <FormControl id="sobreNome"
+                                   placeholder="sobre nome"
+                                   type="text"
+                                   value={values.sobreNome}
+                                   onChange={handleChange}
+                                   onBlur={handleBlur}
+                                   className={
+                                     errors.sobreNome && touched.sobreNome
+                                       ? "text-input error"
+                                       : "text-input"
+                      } />
+                     {errors.sobreNome && touched.sobreNome && (
+                       <Tooltip placement="bottom" className="in" id="tooltip-right">{errors.sobreNome}</Tooltip>
+                    )}
 
-          {/* inicio Password */}
-          <FormGroup controlId="formHorizontalPassword">
-            <Col componentClass={ControlLabel} sm={2}>
-              Senha
-            </Col>
-            <Col sm={10}>
-              <FormControl type="password" placeholder="Password" />
-            </Col>
-          </FormGroup>
-          {/* fim Password */}
-          {/* inicio admin */}
-          <FormGroup>
-            <Col smOffset={0} sm={20}>
-              <Checkbox>Admin?</Checkbox>
-            </Col>
-          </FormGroup>
-          {/* Fim Admin */}
-          <FormGroup>
-            <Col smOffset={0} sm={0}>
-              <Button type="submit" bsStyle="primary" bsSize="large">Salvar</Button> 
-              &nbsp;
-              <Button type="reset" bsStyle="primary" bsSize="large">Limpar</Button>
-            </Col>
-          </FormGroup>
-        </Form>
-        {/* Fim Form */}
-    </div>
-  </div>
-  );
+                    </Col>
+                  </FormGroup>
+                  {/* fim campo Ultimo nome */}
+                  {/* campo usuário */}  
+                    <FormGroup controlId="usuario">
+                        <Col componentClass={ControlLabel} sm={2}>
+                          Usuário
+                        </Col>
+                        <Col sm={10}>
+                          <FormControl id="usuario"
+                                       placeholder="Usuario"
+                                       type="text"
+                                       value={values.usuario}
+                                       onChange={handleChange}
+                                       onBlur={handleBlur}
+                                       className={
+                                         errors.usuario && touched.usuario
+                                           ? "text-input error"
+                                           : "text-input"
+                      }  />
+                       {errors.usuario && touched.usuario && (
+                        <Tooltip placement="bottom" className="in" id="tooltip-right">{errors.usuario}</Tooltip>
+                      )}
+                        </Col>
+                    </FormGroup>
+                  {/* fim campo usário */}
+                  {/* campo email */}  
+                  <FormGroup controlId="email">
+                        <Col componentClass={ControlLabel} sm={2}>
+                          Email
+                        </Col>
+                        <Col sm={10}>
+                          <FormControl id="email"
+                                       placeholder="Email"
+                                       type="text"
+                                       value={values.email}
+                                       onChange={handleChange}
+                                       onBlur={handleBlur}
+                                       className={
+                                         errors.email && touched.email
+                                           ? "text-input error"
+                                           : "text-input"
+                                        }  
+                          />
+                       {errors.email && touched.email && (
+                        <Tooltip placement="bottom" className="in" id="tooltip-right">{errors.email}</Tooltip>
+                      )}
+                        </Col>
+                    </FormGroup>
+                  {/* fim campo email */}
+                      
+                  {/* inicio Password */}
+                      <FormGroup controlId="password">
+                        <Col componentClass={ControlLabel} sm={2}>
+                          Senha
+                        </Col>
+                        <Col sm={10}>
+                          <FormControl id="password"
+                                       placeholder="senha"
+                                       type="password"
+                                       value={values.password}
+                                       onChange={handleChange}
+                                       onBlur={handleBlur}
+                                       className={
+                                       errors.password && touched.password ? (
+                                         'text-input error'
+                                       ) : (
+                                         'text-input'
+                                       )
+                          } />
+                          {errors.password && touched.password && (
+                            <Tooltip placement="bottom" className="in" id="tooltip-right">{errors.password}</Tooltip>
+                          )}
+                        </Col>
+                      </FormGroup>
+                  {/* fim Password */}
+                   {/* inicio Password2 */}
+                   <FormGroup controlId="confirmPassword">
+                        <Col componentClass={ControlLabel} sm={2}>
+                          Confirmação Senha
+                        </Col>
+                        <Col sm={10}>
+                          <FormControl id="confirmPassword"
+                                       placeholder="Confirmar senha"
+                                       type="password"
+                                       value={values.confirmPassword}
+                                       onChange={handleChange}
+                                       onBlur={handleBlur}
+                                       className={
+                                       errors.confirmPassword && touched.confirmPassword ? (
+                                         'text-input error'
+                                       ) : (
+                                         'text-input'
+                                       )
+                          } />
+                          {errors.confirmPassword && touched.confirmPassword && (
+                            <Tooltip placement="bottom" className="in" id="tooltip-right">{errors.confirmPassword}</Tooltip>
+                          )}
+                        </Col>
+                      </FormGroup>
+                  {/* fim Password */}
+                  {/* inicio admin */}
+                       <FormGroup controlId="admin">
+                        <Col smOffset={0} sm={0}>
+                        Admin?
+                        <FormControl id="admin"
+                                     type="checkbox"
+                                      value={values.admin}
+                                      onChange={handleChange}
+                                      onBlur={handleBlur}
+                              
+                           /> 
+                        </Col>
+                      </FormGroup>
+                  {/* fim admin */}
+                  {/* Inicio botoes */}
+                  <FormGroup>
+                    <Col smOffset={0} sm={0}>
+                      <Button type="submit" bsStyle="primary" bsSize="large" disabled={isSubmitting}>Salvar</Button> 
+                      &nbsp;
+                      <Button type="reset" bsStyle="primary" bsSize="large" onClick={handleReset}
+                      disabled={!dirty || isSubmitting} >Limpar</Button>
+                    </Col>
+                  </FormGroup>
+                  {/* fim botoes */}
+              </Form>
+            );
+          }}
+        </Formik>
+        
+        
+        </div>
+      </div>
+    );
+
 }
